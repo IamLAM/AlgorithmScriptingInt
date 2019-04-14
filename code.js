6444522d46671c6edaf0f1a8f0dba30765ea6a71
IamLAM <ingeniero@luismonge.com.mx>
2019-04-13 23:58:53 -0700

    Convert HTML entities

--- code.js
+++ code.js
@@ -2,6 +2,7 @@
 
 function sumAll(arr) {
     var max = Math.max(arr[0], arr[1]);
+    console.log(max);
     var min = Math.min(arr[0], arr[1]);
     var temp = 0;
     for (var i = min; i <= max; i++) {
@@ -11,3 +12,34 @@ function sumAll(arr) {
 }
 
 console.log(sumAll([1, 4]));
+
+function convertHTML(str) {
+    // Split by character to avoid problems.
+
+    var temp = str.split('');
+
+    // Since we are only checking for a few HTML elements I used a switch
+
+    for (var i = 0; i < temp.length; i++) {
+        switch (temp[i]) {
+            case '<':
+                temp[i] = '&lt;';
+                break;
+            case '&':
+                temp[i] = '&amp;';
+                break;
+            case '>':
+                temp[i] = '&gt;';
+                break;
+            case '"':
+                temp[i] = '&quot;';
+                break;
+            case "'":
+                temp[i] = "&apos;";
+                break;
+        }
+    }
+
+    temp = temp.join('');
+    return temp;
+}
